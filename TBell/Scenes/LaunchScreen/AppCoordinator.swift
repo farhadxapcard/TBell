2f0cd3db4a68da5991a105f24404153a0d42bdcd
Farhad Norouzi <fn@xapcard.com>
2019-10-02 13:33:34 -0400

    fixed a bug in app navigation flow

--- TBell/Scenes/LaunchScreen/AppCoordinator.swift
+++ TBell/Scenes/LaunchScreen/AppCoordinator.swift
@@ -34,22 +34,12 @@ class AppCoordinator : Coordinator {
         self.window.rootViewController = self.rootNavigationController
         self.window.makeKeyAndVisible()
         
-        /*
          // create search screen coordinator and add it as our child
          let searchCoordinator = SearchScreenCoordinator(parentCoordinator: self, rootNavigationController: self.rootNavigationController, user: self.user)
          self.add(child: searchCoordinator)
          
          // start it
          searchCoordinator.start(animated: false)
-         */
-        
-        
-        // create VisualizerScreen coordinator
-        let visualizerScreenCoordinator = VisualizerScreenCoordinator(parentCoordinator: self, rootNavigationController: rootNavigationController, user: self.user)
-        self.add(child: visualizerScreenCoordinator)
-        
-        // start it
-        visualizerScreenCoordinator.start(animated: false)
         
     } // end func
 }
